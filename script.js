8e9cdb790241e1c79d5dfe14cf0441b33e6e64c5
Andrés Martinez-Losa <asvimalo@gmail.com>
2021-06-25 10:27:37 +0000

    final code reviewed

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 function compute()
 {
-    var principal = document.getElementById("principal").value;
-    if(principal <= 0){
+    var principal = document.getElementById("principal");
+    var amount = principal.value;
+    if(amount <= 0){
             alert("Enter a positive number");
-            username.focus();
+            principal.focus();
             return false;
     }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
 
-    var interest = (principal * years * rate) /100;
+    var interest = (amount * years * rate) /100;
     
     var year = new Date().getFullYear() + parseInt(years);
     var result = document.getElementById("result");
-    result.innerHTML = "If you deposit " + principal+ "\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year " + year;
+    result.innerHTML = "If you deposit " + amount + "\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year " + year;
 }
 function updateRate(){
     var rateval = document.getElementById("rate").value;
